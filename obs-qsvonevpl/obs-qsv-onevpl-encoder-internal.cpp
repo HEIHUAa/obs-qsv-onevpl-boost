fac553aa630e6ae205df9fe432baf7964ef7ed83
HEIHUAa <112499486+HEIHUAa@users.noreply.github.com>
2026-05-19 06:28:36 +0800

    修复报错

--- obs-qsvonevpl/obs-qsv-onevpl-encoder-internal.cpp
+++ obs-qsvonevpl/obs-qsv-onevpl-encoder-internal.cpp
@@ -643,7 +643,7 @@ struct FieldEntry {
   FieldType type;
 };
 
-static constexpr std::array CO_FIELDS = {
+static constexpr auto CO_FIELDS = std::to_array<FieldEntry>({
   FieldEntry{"CAVLC", offsetof(mfxExtCodingOption, CAVLC), FT_U16},
   {"RateDistortionOpt", offsetof(mfxExtCodingOption, RateDistortionOpt), FT_U16},
   {"MECostType", offsetof(mfxExtCodingOption, MECostType), FT_U16},
@@ -671,7 +671,7 @@ static constexpr std::array CO_FIELDS = {
   {"EndOfStream", offsetof(mfxExtCodingOption, EndOfStream), FT_U16},
 };
 
-static constexpr std::array CO2_FIELDS = {
+static constexpr auto CO2_FIELDS = std::to_array<FieldEntry>({
   FieldEntry{"IntRefType", offsetof(mfxExtCodingOption2, IntRefType), FT_U16},
   {"IntRefCycleSize", offsetof(mfxExtCodingOption2, IntRefCycleSize), FT_U16},
   {"IntRefQPDelta", offsetof(mfxExtCodingOption2, IntRefQPDelta), FT_U16},
@@ -703,7 +703,7 @@ static constexpr std::array CO2_FIELDS = {
   {"FixedFrameRate", offsetof(mfxExtCodingOption2, FixedFrameRate), FT_U16},
 };
 
-static constexpr std::array CO3_FIELDS = {
+static constexpr auto CO3_FIELDS = std::to_array<FieldEntry>({
   FieldEntry{"NumSliceI", offsetof(mfxExtCodingOption3, NumSliceI), FT_U16},
   {"NumSliceP", offsetof(mfxExtCodingOption3, NumSliceP), FT_U16},
   {"NumSliceB", offsetof(mfxExtCodingOption3, NumSliceB), FT_U16},
@@ -747,7 +747,7 @@ static constexpr std::array CO3_FIELDS = {
   {"ExtBrcAdaptiveLTR", offsetof(mfxExtCodingOption3, ExtBrcAdaptiveLTR), FT_U16},
 };
 
-static constexpr std::array CODDI_FIELDS = {
+static constexpr auto CODDI_FIELDS = std::to_array<FieldEntry>({
   FieldEntry{"IntraPredCostType", offsetof(mfxExtCodingOptionDDI, IntraPredCostType), FT_U16},
   {"MEInterpolationMethod", offsetof(mfxExtCodingOptionDDI, MEInterpolationMethod), FT_U16},
   {"MEFractionalSearchType", offsetof(mfxExtCodingOptionDDI, MEFractionalSearchType), FT_U16},
@@ -871,13 +871,13 @@ void QSVEncoder::ParseCustomCodingOptions(const std::string &Options) {
     bool Applied = false;
 
     if (Scope == "CO" && COParams)
-      Applied = ApplyField(COParams, CO_FIELDS, Field, Val);
+      Applied = ApplyField(COParams, std::span<const FieldEntry>(CO_FIELDS), Field, Val);
     else if (Scope == "CO2" && CO2Params)
-      Applied = ApplyField(CO2Params, CO2_FIELDS, Field, Val);
+      Applied = ApplyField(CO2Params, std::span<const FieldEntry>(CO2_FIELDS), Field, Val);
     else if (Scope == "CO3" && CO3Params)
-      Applied = ApplyField(CO3Params, CO3_FIELDS, Field, Val);
+      Applied = ApplyField(CO3Params, std::span<const FieldEntry>(CO3_FIELDS), Field, Val);
     else if (Scope == "CODDI" && CODDIParams)
-      Applied = ApplyField(CODDIParams, CODDI_FIELDS, Field, Val);
+      Applied = ApplyField(CODDIParams, std::span<const FieldEntry>(CODDI_FIELDS), Field, Val);
 
     if (Applied) {
       mfxU16 ParsedVal = ParseCodingOptionValue(Val);
