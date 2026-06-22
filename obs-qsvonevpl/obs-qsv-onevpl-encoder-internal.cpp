19ae66bddaab9f9eabcbbbdb8449fd358570a83e
HEIHUAa <112499486+HEIHUAa@users.noreply.github.com>
2026-06-22 11:46:03 +0800

    更新 obs-qsv-onevpl-encoder-internal.cpp

--- obs-qsvonevpl/obs-qsv-onevpl-encoder-internal.cpp
+++ obs-qsvonevpl/obs-qsv-onevpl-encoder-internal.cpp
@@ -1204,14 +1204,6 @@ mfxStatus QSVEncoder::SetEncoderParams(struct encoder_params *InputParams,
          encodeWidth, encodeHeight);
   }
 
-  QSVEncodeParams.mfx.FrameInfo.Width =
-      static_cast<mfxU16>((((encodeWidth + 15) >> 4) << 4));
-  info("\tWidth: %d", QSVEncodeParams.mfx.FrameInfo.Width);
-
-  QSVEncodeParams.mfx.FrameInfo.Height =
-      static_cast<mfxU16>((((encodeHeight + 15) >> 4) << 4));
-  info("\tHeight: %d", QSVEncodeParams.mfx.FrameInfo.Height);
-
   QSVEncodeParams.mfx.FrameInfo.ChromaFormat =
       static_cast<mfxU16>(InputParams->ChromaFormat);
 
